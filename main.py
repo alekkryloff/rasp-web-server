b86a5ee5ffe80d0dc05535da8309bfffb616d628
a.krylov <zaq06041999>
2021-09-21 14:07:41 +0300

    Added Buttons and Logs

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import datetime
 
 app = Flask(__name__)
+logs = ""
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template("index.html", logs=logs)
+
+
+@app.route('/up', methods=["GET", "POST"])
+def up():
+    global logs
+    logs += str(datetime.datetime.now().time()) + " --> Up\n"
+    return redirect("/")
+
+
+@app.route('/down', methods=["GET", "POST"])
+def down():
+    global logs
+    logs += str(datetime.datetime.now().time()) + " --> Down\n"
+    return redirect("/")
 
 
 if __name__ == '__main__':
---
+++ static/autoscroll.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var textarea = document.getElementById('textarea');
+    textarea.scrollTop = textarea.scrollHeight;
+};
